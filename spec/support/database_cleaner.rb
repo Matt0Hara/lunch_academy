95391cd58194a53640caa7d327486a643a3b1caa
Matthew O'Hara <matthewohara@Matthews-MacBook-Air.local>
2015-10-08 13:49:21 -0400

    finished infinite scroll, added some styling

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,21 +1,21 @@
 RSpec.configure do |config|
   config.before(:suite) do
-   DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.before(:each) do
-   DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :transaction
   end
 
   config.before(:each, js: true) do
-   DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.strategy = :truncation
   end
 
   config.before(:each) do
-   DatabaseCleaner.start
+    DatabaseCleaner.start
   end
 
   config.after(:each) do
-   DatabaseCleaner.clean
+    DatabaseCleaner.clean
   end
 end
